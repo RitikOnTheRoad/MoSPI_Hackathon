e169c2eb95dfca1b1e24f44e83b412bca9aa2120
sakunisgithub <ananda2003biswas@gmial.com>
2025-03-31 03:57:15 +0530

    added percentages

--- Ananda/4/3/4-3.R
+++ Ananda/4/3/4-3.R
@@ -19,10 +19,15 @@ df[x] <- 0
 for(i in 1:nrow(df)){
   
   jpeg(paste("D:\\Users\\Documents\\MOSPI_hackathon\\Ananda\\4\\3\\", df[i, 1], ".jpg", sep = ""), 
-       width = 6000, height = 4000, 
+       width = 7000, height = 4000, 
        res = 500)
 
-  pie(as.numeric(df[i, -1]), names(df)[-1],
+  values <- as.numeric(df[i, -1])
+  
+  percentages <- round(( values / sum(values) ) * 100, digits = 2)
+  
+  pie(values, 
+      paste(names(df)[-1], ":", percentages, "%"),
       col = c("#FF5733", "#33FF57", "#3357FF", "#FF33A8", "#FFC733", "#33FFF5", "#A833FF"),
       main = paste("Employment Distribution in", df[i, 1]))
       
